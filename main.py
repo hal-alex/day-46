7c291ae00f1e7ee1c093ff2f495549edf8c14cb9
hal-alex <102769562+hal-alex@users.noreply.github.com>
2022-06-03 14:23:09 +0100

    Searching Spotify

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
 
 user_year = input("Which year of music would you like to listen to? Use format YYYY-MM-DD: ")
 
@@ -15,4 +18,25 @@ song_names = soup.find_all(name="h3", class_="a-no-trucate")
 
 song_names_list = [song.getText().strip() for song in song_names]
 print(song_names_list)
+
+SPOTIFY_CLIENT_ID = "b4d90e186db64bc4b76cbebceedfdcbf"
+SPOTIFY_SECRET = "4c273f9e1ff34359830edb43a85623e5"
+REDIRECT_URI = "https://example.com/"
+
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        scope="playlist-modify-private",
+        redirect_uri=REDIRECT_URI,
+        client_id=SPOTIFY_CLIENT_ID,
+        client_secret=SPOTIFY_SECRET,
+        show_dialog=True,
+        cache_path="token.txt"
+    )
+)
+
+user_id = sp.current_user()["id"]
+
+for song in song_names_list:
+    result = sp.search(q=f"track:{song}", type="track")
+    print(result)
 
